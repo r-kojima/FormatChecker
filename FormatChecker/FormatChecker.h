539b0064529c70c59e1881373635ff510c64657d
ryo_kojima <kojimar24@gmail.com>
2020-08-27 22:15:39 +0900

    Add Project

---
+++ FormatChecker/FormatChecker.h
@@ -0,0 +1,19 @@
+//
+//  FormatChecker.h
+//  FormatChecker
+//
+//  Created by 小嶌諒 on 2020/08/27.
+//  Copyright © 2020 r-kojima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FormatChecker.
+FOUNDATION_EXPORT double FormatCheckerVersionNumber;
+
+//! Project version string for FormatChecker.
+FOUNDATION_EXPORT const unsigned char FormatCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormatChecker/PublicHeader.h>
+
+
